168eea7c6890378c3365bb78fc531fe0b065eca2
Kroese <kroese@users.noreply.github.com>
2024-02-10 13:21:16 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-[ -n "$DNS1" ] && sed -i -e "s/1.0.0.1/$DNS1/g" /etc/dnsmasq.conf
-[ -n "$DNS2" ] && sed -i -e "s/1.1.1.1/$DNS2/g" /etc/dnsmasq.conf
+conf="/etc/dnsmasq.conf"
 
-exec dnsmasq --no-daemon --user=dnsmasq --group=dnsmasq
+if [ ! -f "$conf" ]; then
+  conf="/etc/dnsmasq.custom"
+  cp /etc/dnsmasq.default "$conf"
+  [ -n "$DNS1" ] && sed -i -e "s/1.0.0.1/$DNS1/g" "$conf"
+  [ -n "$DNS2" ] && sed -i -e "s/1.1.1.1/$DNS2/g" "$conf"
+fi
+
+exec dnsmasq "--conf-file=$conf" --no-daemon --user=dnsmasq --group=dnsmasq
