c577055718b8b56df57fc2880b2fb363ba464985
Kroese <kroese@users.noreply.github.com>
2024-02-10 13:25:04 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -5,6 +5,7 @@ conf="/etc/dnsmasq.conf"
 
 if [ ! -f "$conf" ]; then
   conf="/etc/dnsmasq.custom"
+  rm -f "$conf"
   cp /etc/dnsmasq.default "$conf"
   [ -n "$DNS1" ] && sed -i -e "s/1.0.0.1/$DNS1/g" "$conf"
   [ -n "$DNS2" ] && sed -i -e "s/1.1.1.1/$DNS2/g" "$conf"
