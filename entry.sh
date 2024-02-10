464883207b08c75023ebb95cd8f2f5591993cf74
Kroese <kroese@users.noreply.github.com>
2024-02-10 12:41:15 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+sed -i -e "s/1.0.0.1/$DNS1/g" /etc/dnsmasq.conf
+sed -i -e "s/1.1.1.1/$DNS2/g" /etc/dnsmasq.conf
+
 exec dnsmasq --no-daemon --user=dnsmasq --group=dnsmasq
