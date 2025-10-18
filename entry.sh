02fad80492aae31134a48a3b75efa59174685991
Kroese <kroese@users.noreply.github.com>
2025-10-18 23:11:39 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -10,44 +10,35 @@ if [ -d "$conf" ]; then
 fi
 
 if [ ! -f "$conf" ]; then
+
   conf="/etc/dnsmasq.custom"
   template="/etc/dnsmasq.default"
-  
+
   if [ ! -f "$template" ]; then
     echo "Your /etc directory does not contain a valid dnsmasq.conf file!"
     exit 1
   fi
-  
+
   rm -f "$conf"
   cp "$template"  "$conf"
+
+  [ -n "${DNS1:-}" ] && echo "server=$DNS1" >> "$conf"
+  [ -n "${DNS2:-}" ] && echo "server=$DNS2" >> "$conf"
+  [ -n "${DNS3:-}" ] && echo "server=$DNS3" >> "$conf"
+  [ -n "${DNS4:-}" ] && echo "server=$DNS4" >> "$conf"
   
-  if [ -n "$DNS1" ]; then
-    echo "server=$DNS1" >> "$conf"
-  fi
-  
-  if [ -n "$DNS2" ]; then
-    echo "server=$DNS2" >> "$conf"
-  fi
-  
-  if [ -n "$DNS3" ]; then
-    echo "server=$DNS3" >> "$conf"
-  fi
-  
-  if [ -n "$DNS4" ]; then
-    echo "server=$DNS4" >> "$conf"
-  fi
-  
-  if [ -n "$CACHE_SIZE" ]; then
+  if [ -n "${CACHE_SIZE:-}" ]; then
     sed -i -e "s/^cache-size=.*/cache-size=$CACHE_SIZE/g" "$conf"
   fi
   
-  if [ "$DOMAIN_NEEDED" = "true" ]; then
+  if [ -n "${DOMAIN_NEEDED:-}" ]; then
     sed -i -e "s/^#domain-needed/domain-needed/g" "$conf"
   fi
   
-  if [ "$LOG_QUERIES" = "true" ]; then
+  if [ -n "${LOG_QUERIES:-}" ]; then
     sed -i -e "s/^#log-queries/log-queries/g" "$conf"
   fi
+
 fi
 
 exec dnsmasq "--conf-file=$conf" --no-daemon --no-resolv
