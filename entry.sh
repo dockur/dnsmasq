44077ccc24b4bac4d90a85b3cc8aa96b4a3a93d6
Iven <99438726+ivenos@users.noreply.github.com>
2025-10-18 23:21:12 +0200

    feat: Add additional configuration options (#14)

--- entry.sh
+++ entry.sh
@@ -5,10 +5,8 @@ conf="/etc/dnsmasq.conf"
 
 # Check if config file is not a directory
 if [ -d "$conf" ]; then
-
     echo "The bind $conf maps to a file that does not exist!"
     exit 1
-
 fi
 
 if [ ! -f "$conf" ]; then
@@ -24,8 +22,22 @@ if [ ! -f "$conf" ]; then
   rm -f "$conf"
   cp "$template"  "$conf"
 
-  [ -n "$DNS1" ] && sed -i -e "s/1.0.0.1/$DNS1/g" "$conf"
-  [ -n "$DNS2" ] && sed -i -e "s/1.1.1.1/$DNS2/g" "$conf"
+  [ -n "${DNS1:-}" ] && echo "server=$DNS1" >> "$conf"
+  [ -n "${DNS2:-}" ] && echo "server=$DNS2" >> "$conf"
+  [ -n "${DNS3:-}" ] && echo "server=$DNS3" >> "$conf"
+  [ -n "${DNS4:-}" ] && echo "server=$DNS4" >> "$conf"
+  
+  if [ -n "${CACHE_SIZE:-}" ]; then
+    sed -i -e "s/^cache-size=.*/cache-size=$CACHE_SIZE/g" "$conf"
+  fi
+  
+  if [ -n "${DOMAIN_NEEDED:-}" ]; then
+    sed -i -e "s/^#domain-needed/domain-needed/g" "$conf"
+  fi
+  
+  if [ -n "${LOG_QUERIES:-}" ]; then
+    sed -i -e "s/^#log-queries/log-queries/g" "$conf"
+  fi
 
 fi
 
